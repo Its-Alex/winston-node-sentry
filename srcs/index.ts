439515110c4104427ce4a76d0500b64109dc621a
ItsAlex <me@itsalex.fr>
2019-02-07 15:57:21 +0100

    feat: prevent some case to break logging and add default value

--- srcs/index.ts
+++ srcs/index.ts
@@ -2,39 +2,46 @@ import _ from 'lodash'
 import Transport from 'winston-transport'
 import * as Sentry from '@sentry/node'
 
-interface Options {
+interface WinstonSentryOptions {
   level?: string
   sentry: Sentry.NodeOptions
   sentryScope?(scope: Sentry.Scope): void 
 }
 
 class SentryTransport extends Transport {
-  constructor(opts: Options) {
+  constructor(opts: WinstonSentryOptions) {
     super(opts)
     opts = _.defaultsDeep(opts, {
       level: 'error',
+      sentry: {
+        attachStacktrace: true,
+        sendDefaultPii: true,
+        integrations: [
+          new Sentry.Integrations.Modules,
+          new Sentry.Integrations.Transaction
+        ]
+      }
     })
 
     this.level = opts.level
     if (_.isFunction(opts.sentryScope)) Sentry.configureScope(opts.sentryScope)
-    else Sentry.configureScope(this._configureScope)
 
     Sentry.init(opts.sentry)
   }
 
   log(info: any, callback: Function) {
+    setImmediate(() => this.emit('logged', info))
     // Some attribute is not in type
     let self = this as any
 
     if (self.levels[info.level] <= self.levels[this.level!]) {
-      Sentry.captureException(info.message)
+      if (_.isError(info)) {
+        Sentry.captureException(info)
+      } else if (_.isError(info.message)) {
+        Sentry.captureException(info.message)
+      }
     }
-
-    callback();
-  }
-
-  _configureScope(scope: Sentry.Scope) {
-    scope.setLevel(this.level as Sentry.Severity)
+    callback()  
   }
 }
 
